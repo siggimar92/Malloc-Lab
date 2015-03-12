e511ff6645908c9d13ec54b92c6ccfa5cd489721
Haffi <hafthort12@ru.is>
2015-03-12 14:00:19 +0000

    hmm

--- mm.c
+++ mm.c
@@ -119,8 +119,8 @@ team_t team = {
 
 // #define NEXT_FREE(bp)  ((char*)(bp) + WSIZE)
 // #define PREV_FREE(bp)  ((char*)(bp))
-#define NEXT_FREE(bp) ((char*)(bp+DSIZE))
-#define PREV_FREE(bp) ((char*)(bp+WSIZE))
+#define NEXT_FREE(bp) ((void*)((char*)(bp+DSIZE)))
+#define PREV_FREE(bp) ((void*)((char*)(bp+WSIZE)))
 
 /* $end mallocmacros */
 
