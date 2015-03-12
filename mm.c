1bdb7ebc3d1b1466b226b23a95672114fbd57098
Haffi <hafthort12@ru.is>
2015-03-12 13:38:18 +0000

    test test test

--- mm.c
+++ mm.c
@@ -119,8 +119,8 @@ team_t team = {
 
 // #define NEXT_FREE(bp)  ((char*)(bp) + WSIZE)
 // #define PREV_FREE(bp)  ((char*)(bp))
-#define NEXT_FREE(bp) ((void *)* (bp+WSIZE))
-#define PREV_FREE(bp) ((void *)* (bp))
+#define NEXT_FREE(bp) ((void *)(bp+WSIZE))
+#define PREV_FREE(bp) ((void *)(bp))
 
 /* $end mallocmacros */
 
