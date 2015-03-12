277044388ff9d40395d6aa1e1d1e67e2f8181f41
Haffi <hafthort12@ru.is>
2015-03-12 22:52:35 +0000

    hmm

--- mm.c
+++ mm.c
@@ -461,35 +461,56 @@ void *mm_realloc(void *ptr, size_t size)
  */
 static void *coalesce(void *bp) 
 {
-    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    // printf("\nBefore Coalesce\n");
+    //mm_checkheap(VERBOSE);
+
+    //size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp))) || PREV_BLKP(bp) == bp;
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
     size_t size = GET_SIZE(HDRP(bp));
 
-    if (prev_alloc && next_alloc) {            /* Case 1 - both */
+    /* Case 1 - both prev and next are alloceted, nothing to be done */
+    if (prev_alloc && next_alloc) {
         return bp;
     }
 
-    else if (prev_alloc && !next_alloc) {      /* Case 2 */
+    /* Case 2 - next block to the right can be merged */
+    else if (prev_alloc && !next_alloc) {
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        //removeFree(NEXT_BLKP(bp));
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size,0));
     }
 
-    else if (!prev_alloc && next_alloc) {      /* Case 3 */
+    /* Case 3 - previous block to the left can be merged */
+    else if (!prev_alloc && next_alloc) {
         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        //removeFree(PREV_BLKP(bp));
+        //PUT(FTRP(bp), PACK(size, 0));
+        PUT(FTRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+
+    /* Case 4 - Both blocks can be merged */
+    else {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+
+        // removeFree(NEXT_BLKP(bp));
+        // removeFree(PREV_BLKP(bp));
+
+        //PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        //PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        // bp = PREV_BLKP(bp);
+        bp = PREV_BLKP(bp);
+        PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        bp = PREV_BLKP(bp);
     }
 
-    else {                                     /* Case 4 */
-        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + 
-            GET_SIZE(FTRP(NEXT_BLKP(bp)));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
-        bp = PREV_BLKP(bp);
-    }
+    // printf("After Coalesce\n");
+    //mm_checkheap(VERBOSE);
 
+    //insertFree(bp);
     return bp;
 }
 
